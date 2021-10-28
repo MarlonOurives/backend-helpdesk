95224053eafb24480d2a03d41eb4422f182af9c0
Marlon Ourives <marloncicero_araujo@hotmail.com>
2021-10-28 10:50:03 -0400

    carga inicial da base de dados

--- src/main/java/com/marlon/helpdesk/HelpdeskApplication.java
+++ src/main/java/com/marlon/helpdesk/HelpdeskApplication.java
@@ -1,13 +1,52 @@
 package com.marlon.helpdesk;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class HelpdeskApplication {
+import com.marlon.helpdesk.domain.Chamado;
+import com.marlon.helpdesk.domain.Cliente;
+import com.marlon.helpdesk.domain.Tecnico;
+import com.marlon.helpdesk.domain.enums.Perfil;
+import com.marlon.helpdesk.domain.enums.Prioridade;
+import com.marlon.helpdesk.domain.enums.Status;
+import com.marlon.helpdesk.repositories.ChamadoRepository;
+import com.marlon.helpdesk.repositories.ClienteRepository;
+import com.marlon.helpdesk.repositories.TecnicoRepository;
 
+@SpringBootApplication
+public class HelpdeskApplication implements CommandLineRunner{
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+	@Autowired
+	private ChamadoRepository chamadoRepository;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HelpdeskApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Tecnico tec1 = new Tecnico(null, "Marlon", "96632193005", "marlon@mail.com", "123");
+		tec1.addPerfil(Perfil.ADMIN);
+		
+		Cliente cli1 = new Cliente(null, "Roberto", "85545547010", "roberto@mail.com", "123");
+		
+		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 1", "Primeiro chamado", tec1, cli1);
+		
+		tecnicoRepository.saveAll(Arrays.asList(tec1));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		chamadoRepository.saveAll(Arrays.asList(c1));
+		
+		
 	}
 
 }
