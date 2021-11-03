712327975ced3944fe6da5d2009b8cb3b4541eeb
Marlon Ourives <marloncicero_araujo@hotmail.com>
2021-11-03 16:35:56 -0400

    findAll para chamado

--- src/main/java/com/marlon/helpdesk/resources/ChamadoResources.java
+++ src/main/java/com/marlon/helpdesk/resources/ChamadoResources.java
@@ -1,5 +1,8 @@
 package com.marlon.helpdesk.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,10 +20,18 @@ public class ChamadoResources {
 
 	@Autowired
 	private ChamadoService service;
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id){
+	public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id) {
 		Chamado obj = service.findById(id);
 		return ResponseEntity.ok().body(new ChamadoDTO(obj));
+	}
+
+	@GetMapping
+	public ResponseEntity<List<ChamadoDTO>> findAll() {
+		List<Chamado> list = service.findAll();
+		List<ChamadoDTO> listDTO = list.stream().map(obj -> new ChamadoDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+
 	}
 }
