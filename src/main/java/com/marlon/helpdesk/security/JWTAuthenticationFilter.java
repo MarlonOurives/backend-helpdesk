95089aeddcb2932039c43bc71fbc2fd72b951f66
Marlon Ourives <marloncicero_araujo@hotmail.com>
2021-11-08 20:42:44 -0400

    corrigindo illegal escape character

--- src/main/java/com/marlon/helpdesk/security/JWTAuthenticationFilter.java
+++ src/main/java/com/marlon/helpdesk/security/JWTAuthenticationFilter.java
@@ -63,8 +63,13 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
 	private CharSequence json() {
 		long date = new Date().getTime();
-		return "{" + "\timestamp\":" + date + ", " + "\status\":401, " + "\"error\": \"Não autorizado\", "
-				+ "\"message\":\"Email ou senha inválidos\"," + "\"path\":\"/login\"}";
+		return "{"
+				+ "\"timestamp\": " + date + ", " 
+				+ "\"status\": 401, "
+				+ "\"error\": \"Não autorizado\", "
+				+ "\"message\": \"Email ou senha inválidos\", "
+				+ "\"path\": \"/login\"}";
 	}
+	
 
 }
